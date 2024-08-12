4b8f618ef9637779ed3b847ce9b5e943076aa9bd
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-12 23:20:11 +0530

    market bar

--- frontend/app/trade/[market]/page.tsx
+++ frontend/app/trade/[market]/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { MarketBar } from "@/app/components/MarketBar";
+import { useParams } from "next/navigation";
+
 export default function Page() {
-    return  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Trade
-  </main>
+  const { market } = useParams();
+
+  return (
+    <div className="flex flex-row flex-1">
+      <div className="flex flex-col flex-1">
+        <MarketBar market={market as string} />
+        <div className="flex flex-row h-[920px] border-y border-slate-800">
+          <div className="flex flex-col flex-1">
+            TradeView
+          </div>
+          <div className="flex flex-col w-[250px] overflow-hidden">
+            Depth
+          </div>
+        </div>
+      </div>
+      <div className="w-[10px] flex-col border-slate-800 border-l"></div>
+      <div>
+        <div className="flex flex-col w-[250px]">
+          SwapUI
+        </div>
+      </div>
+    </div>
+  );
 }
