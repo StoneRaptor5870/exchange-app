db58938d0d6465dadae0ce5a2981f649b61fcf95
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-13 00:16:07 +0530

    swap ui, trade view candles

--- frontend/app/trade/[market]/page.tsx
+++ frontend/app/trade/[market]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { MarketBar } from "@/app/components/MarketBar";
+import { SwapUI } from "@/app/components/SwapUI";
+import { TradeView } from "@/app/components/TradeView";
 import { useParams } from "next/navigation";
 
 export default function Page() {
@@ -12,7 +14,7 @@ export default function Page() {
         <MarketBar market={market as string} />
         <div className="flex flex-row h-[920px] border-y border-slate-800">
           <div className="flex flex-col flex-1">
-            TradeView
+            <TradeView market={market as string} />
           </div>
           <div className="flex flex-col w-[250px] overflow-hidden">
             Depth
@@ -22,7 +24,7 @@ export default function Page() {
       <div className="w-[10px] flex-col border-slate-800 border-l"></div>
       <div>
         <div className="flex flex-col w-[250px]">
-          SwapUI
+          <SwapUI market={market as string} />
         </div>
       </div>
     </div>
